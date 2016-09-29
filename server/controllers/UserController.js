5cf929ea92fc06c6a033f936ebb35837ed90ec6a
Jake Miller <jacobkmiller@icloud.com>
2016-09-28 21:01:08 -0700

    temp

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -135,7 +135,7 @@ module.exports = {
   updateInstagram: function(req, res){
     console.log(req);
     var temp = Url.parse(req.url)
-    var token = temp.hash;
+    var token = temp;
     Group.findOne({_id:req.session.group._id}, function(err, group){
       if(err){
         console.log("failed to find group in updateInstagram" + err);

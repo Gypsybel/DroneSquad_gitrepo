0f8d3a8969566293289ae370e34ff52421bf84eb
Jake Miller <jacobkmiller@icloud.com>
2016-09-28 22:12:54 -0700

    ?

--- server/config/routes.js
+++ server/config/routes.js
@@ -14,7 +14,7 @@ module.exports = function(app){
   app.get('/validateLoggedIn', userRoutes.getLoggedUser)
   app.post('/addmeetup/:id', userRoutes.addMeetup);
   app.post('/addgroup', userRoutes.createGroup);
-  app.get('/instagram/:code', userRoutes.updateInstagram);
+  app.get('/instagram/?:code', userRoutes.updateInstagram);
 }
 function userAuth(req,res,next){
   if (req.session.user){

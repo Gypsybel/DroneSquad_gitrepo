de0659b24f2dbd8a0539dd80cde31af6e65498f3
Ethan <Ethan@ethansaks.com>
2016-09-29 14:11:40 -0700

    deleted vegas

--- client/static/controllers/homeController.js
+++ client/static/controllers/homeController.js
@@ -1,10 +1,13 @@
 app.controller('homeController',[ '$scope', '$http','userFactory','$location', 'NgMap', function($scope, $http, userFactory, $location, NgMap){
 	NgMap.getMap().then(function(map){
-		console.log(map.getCenter());
+	map.getCenter();
 	})
 	$scope.locations = [
-		{coordinate_x: "34.157924", coordinate_y:"-118.166216"},
-		{coordinate_x: "34.270544", coordinate_y:"-118.378726"}
+		{coordinates: "34.0697535999999985, -118.362128600000005"},
+		{coordinates: "34.157924, -118.166216"},
+		{coordinates: "34.270544, -118.378726"},
+		{coordinates: "34.1790877999999978, -118.319483000000005"},
+		{coordinates: "34.1144110400000002, -118.157018629999996"},
 	]
 
 
